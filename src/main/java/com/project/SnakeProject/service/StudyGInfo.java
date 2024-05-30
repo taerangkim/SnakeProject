744e305d21d0f63a146cd9a3af317e03df2c78ff
muse7244 <taerangkim0116@gmail.com>
2024-05-30 23:33:19 +0900

    main

--- src/main/java/com/project/SnakeProject/service/StudyGInfo.java
+++
@@ -1,9 +0,0 @@
-package com.project.SnakeProject.service;
-
-import com.project.SnakeProject.vo.StudyGInfoVo;
-
-import java.util.List;
-
-public interface StudyGInfo {
-  public List<StudyGInfoVo> ViewStudyGInfo();
-}
